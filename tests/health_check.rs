e5dfcbe24dfd3a913c4424b8929daf6eee483f39
Martin Kustra <martin.kustra@tutanota.com>
2023-03-02 19:01:45 +0100

    ch3.8.5.2: codebase split

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,7 +6,7 @@ use std::net::TcpListener;
 fn spawn_app() -> String {
     let tcp_listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
     let address = tcp_listener.local_addr().unwrap().to_string();
-    let server = zero2prod::run(tcp_listener).expect("Failed to bind address.");
+    let server = zero2prod::startup::run(tcp_listener).expect("Failed to bind address.");
 
     tokio::spawn(server);
 
