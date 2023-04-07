b5ea8eda9572b9140096774e0674c96821a5742c
Martin Kustra <martin.kustra@tutanota.com>
2023-04-07 14:56:47 +0200

    ch4.4: add 'log' create and add logs to subscriptions handler

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(Env::default().default_filter_or(log::Level::Info.as_str()))
+        .init();
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,14 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: Form<FormData>, connection_pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    // careful with GDPR, name and email are considered Personal Identifiable Information
+    log::info!(
+        "request_id {} - Persisting new subscriber into database with name='{}' and email='{}'.",
+        request_id,
+        form.name,
+        form.email
+    );
     match sqlx::query!(
         r#"
             INSERT INTO t_subscriptions (id, email, name, subscribed_at)
@@ -26,9 +34,16 @@ pub async fn subscribe(form: Form<FormData>, connection_pool: web::Data<PgPool>)
     .execute(connection_pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("request_id {} - New subscriber persisted.", request_id);
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!(
+                "request_id {} - Failed to persist new subscriber: {:?}",
+                request_id,
+                e
+            ); // careful with GDPR, this logs email when unique constraint errors
             HttpResponse::InternalServerError().finish()
         }
     }
