80345148166efb99ef7cbf3bc1147b110f94dbc6
Kattair <martin.kustra@tutanota.com>
2024-02-25 17:03:17 +0000

    ch7.7.3: Pending confirmation

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -23,14 +23,30 @@ async fn subscribe_returns_200_when_valid_form_data() {
     let response = app.post_subscriptions(body.into()).await;
 
     assert_eq!(StatusCode::OK, response.status());
+}
 
-    let saved = sqlx::query!("SELECT email, name FROM t_subscriptions")
+#[tokio::test]
+pub async fn subscribe_persists_new_subscriber_in_database() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path(""))
+        .and(method(Method::Post))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let _ = app.post_subscriptions(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM t_subscriptions")
         .fetch_one(&app.connection_pool)
         .await
-        .expect("Failed to fetch saved subscription");
+        .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
@@ -95,7 +111,7 @@ async fn subscribe_sends_confirmation_email_for_valid_data() {
 }
 
 #[tokio::test]
-async fn subscribe_send_a_confirmation_email_with_a_link() {
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
