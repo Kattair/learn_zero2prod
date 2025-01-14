534ff74e3b3bf472519895b089e755f9f604d357
Kattair <martin.kustra@tutanota.com>
2025-01-14 08:17:43 +0100

    ch10.7.5.3: Typed Session

--- src/routes/login/mod.rs
+++ src/routes/login/mod.rs
@@ -1,6 +1,5 @@
 use std::fmt::{self, Write};
 
-use actix_session::Session;
 use actix_web::{
     error::InternalError,
     http::header::{self, ContentType},
@@ -15,6 +14,7 @@ use sqlx::PgPool;
 use crate::{
     authentication::{self, AuthError, Credentials},
     error::error_chain_fmt,
+    session_state::TypedSession,
 };
 
 #[derive(serde::Deserialize, Debug)]
@@ -56,7 +56,7 @@ pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
 pub async fn login(
     form: web::Form<LoginFormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -68,7 +68,7 @@ pub async fn login(
             tracing::Span::current().record("user_id", tracing::field::display(&user_id));
             session.renew();
             session
-                .insert("user_id", user_id)
+                .insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
