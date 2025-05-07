e1714ac8875d26be187ddb7af68b7be73c857399
Kattair <martin.kustra@tutanota.com>
2025-05-07 17:10:26 +0200

    ch11.10: Dealing With Errors - Specification

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,10 +1,14 @@
 use std::time::Duration;
 
+use fake::{
+    faker::{internet::en::SafeEmail, name::en::Name},
+    Fake,
+};
 use reqwest::StatusCode;
 use wiremock::{
     http::Method,
     matchers::{any, method, path},
-    Mock, ResponseTemplate,
+    Mock, MockBuilder, ResponseTemplate,
 };
 
 use crate::helpers::{assert_is_redirect_to, spawn_app, ConfirmationLinks, TestApp};
@@ -16,9 +20,8 @@ pub async fn newsletter_creation_is_idempotent() {
     create_confirmed_subscriber(&app).await;
     app.login_test_user().await;
 
-    Mock::given(path(""))
-        .and(method(Method::Post))
-        .respond_with(ResponseTemplate::new(200))
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(StatusCode::OK))
         .expect(1)
         .mount(&app.email_server)
         .await;
@@ -56,10 +59,9 @@ pub async fn concurrent_form_submission_is_handled_gracefully() {
     create_confirmed_subscriber(&app).await;
     app.login_test_user().await;
 
-    Mock::given(path(""))
-        .and(method(Method::Post))
+    when_sending_an_email()
         // Delay to make it possible for a second request to arrive before the first one completes
-        .respond_with(ResponseTemplate::new(200).set_delay(Duration::from_secs(2)))
+        .respond_with(ResponseTemplate::new(StatusCode::OK).set_delay(Duration::from_secs(2)))
         .expect(1)
         .mount(&app.email_server)
         .await;
@@ -83,6 +85,53 @@ pub async fn concurrent_form_submission_is_handled_gracefully() {
     );
 
     // Mock verifies on Drop that we have sent the newsletter email **once**
+}
+
+#[tokio::test]
+pub async fn transient_errors_do_not_cause_duplicate_deliveries_on_retries() {
+    // Arrange
+    let app = spawn_app().await;
+    let form_data = format!(
+        "title=Newsletter%20title\
+        &plaintext=Newsletter%20body%20as%20plain%20text\
+        &html=<p>Newsletter%20body%20as%20HTML</p>\
+        &idempotency_key={}",
+        uuid::Uuid::new_v4().to_string()
+    );
+
+    create_confirmed_subscriber(&app).await;
+    create_confirmed_subscriber(&app).await;
+    app.login_test_user().await;
+
+    // Act - Part 1 - Submit newsletter form
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(StatusCode::OK))
+        .up_to_n_times(1)
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(StatusCode::INTERNAL_SERVER_ERROR))
+        .up_to_n_times(1)
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let response = app.post_newsletters(form_data.clone()).await;
+    assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
+
+    // Act - Part 2 - Retry submitting form
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(StatusCode::OK))
+        .expect(1)
+        .named("Delivery retry")
+        .mount(&app.email_server)
+        .await;
+
+    let response = app.post_newsletters(form_data.clone()).await;
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    // Mock verifies on Drop that we did not send duplicates
 }
 
 #[tokio::test]
@@ -132,7 +181,7 @@ pub async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     create_unconfirmed_subscriber(&app).await;
 
     Mock::given(any())
-        .respond_with(ResponseTemplate::new(200))
+        .respond_with(ResponseTemplate::new(StatusCode::OK))
         .expect(0) // assert no request is fired against Mailtrap
         .mount(&app.email_server)
         .await;
@@ -157,7 +206,7 @@ pub async fn newsletters_are_delivered_to_confirmed_subscribers() {
     create_confirmed_subscriber(&app).await;
 
     Mock::given(any())
-        .respond_with(ResponseTemplate::new(200))
+        .respond_with(ResponseTemplate::new(StatusCode::OK))
         .expect(1) // assert a single request is fired against Mailtrap
         .mount(&app.email_server)
         .await;
@@ -175,6 +224,10 @@ pub async fn newsletters_are_delivered_to_confirmed_subscribers() {
     assert_is_redirect_to(&response, "/admin/newsletters");
 }
 
+fn when_sending_an_email() -> MockBuilder {
+    Mock::given(path("")).and(method(Method::Post))
+}
+
 async fn create_confirmed_subscriber(app: &TestApp) {
     let confirmation_links = create_unconfirmed_subscriber(app).await;
 
@@ -186,18 +239,22 @@ async fn create_confirmed_subscriber(app: &TestApp) {
 }
 
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let name: String = Name().fake();
+    let email: String = SafeEmail().fake();
+    let body = format!("name={name}&email={email}")
+        .replace(" ", "%20")
+        .replace("@", "%40");
 
     // prevent trying to send a confirmation email to Mailtrap
     let _mock_guard = Mock::given(method(Method::Post))
         .and(path(""))
-        .respond_with(ResponseTemplate::new(200))
+        .respond_with(ResponseTemplate::new(StatusCode::OK))
         .named("Create unconfirmed subscriber")
         .expect(1)
         .mount_as_scoped(&app.email_server)
         .await;
 
-    app.post_subscriptions(body.into())
+    app.post_subscriptions(body.to_string())
         .await
         .error_for_status()
         .unwrap();
